0c83bb36b3def69d53c1a66fabc2e69abb897197
Rasamizafy <rasamizafybryan98@gmail.com>
2020-07-27 17:02:49 +0200

    sendmail

--- sendmail.php
+++ sendmail.php
@@ -5,17 +5,7 @@ if(isset($_POST['mail'])){
     $header.='Content-Type:text/html; charset="uft-8"'."\n";
     $header.='Content-Transfer-Encoding: 8bit';
                                 
-    $message='
-    <html>
-        <body>
-            <div align="center">
-                Félicitation' . $pseudo . '<br />
-                Bienvenue sur le site JEPSENS BRITE.
-                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpagtour.info%2Fla-justice-europeenne-se-penche-sur-le-pouce-bleu%2F&psig=AOvVaw24WbFuewyeDCmh1udyzMv5&ust=1595683016967000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCQ08385eoCFQAAAAAdAAAAABAD"/>
-            </div>
-        </body>
-    </html>
-    ';
+    $message='Salut ça va?';
                                 
     mail("rasamizafybryan98@gmail.com", "Salut tout le monde !", $message, $header);
 }
