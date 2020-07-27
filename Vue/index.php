928882ffff59dcf2eaf40da12efa0a82655d43d8
Mathieu Bourlard <mathieubourlard01@gmail.com>
2020-07-27 13:25:10 +0200

    correction

--- Vue/index.php
+++ Vue/index.php
@@ -48,7 +48,7 @@
         <i class="fas fa-trash buttonsection"></i>
 
         <img src="téléchargement.jpg" alt="image event" class="imgevent">
-        <h2 class="Titre-h2">Bassleader reactivated world <div class="category">concert</div></h2>
+        <h2 class="titre-h2">Bassleader reactivated world <div class="category">concert</div></h2>
         <h5 class="date"><?php echo date('H:i, D d/m/Y')?></h5>
         <p class="description"><strong>description:</strong>
          Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
@@ -61,7 +61,7 @@
         <i class="fas fa-trash buttonsection"></i>
 
         <img src="2016-Daydream-Festival.jpg" alt="image event" class="imgevent">
-        <h2 class="Titre-h2">Daydream festival<div class="category">concert</div></h2>
+        <h2 class="titre-h2">Daydream festival<div class="category">concert</div></h2>
         <h5 class="date"><?php echo date('H:i, D d/m/Y')?></h5>
         <p class="description"><strong>description:</strong> Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
         <p class="author">auteur</p>
@@ -73,7 +73,7 @@
         <i class="fas fa-trash buttonsection"></i>
 
         <img src="dominator.jpg" alt="image event" class="imgevent">
-        <h2 class="Titre-h2">Dominator 2020<div class="category">concert</div></h2>
+        <h2 class="titre-h2">Dominator 2020<div class="category">concert</div></h2>
         <h5 class="date"><?php echo date('H:i, D d/m/Y')?></h5>
         <p class="description"><strong>description:</strong> Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
         <p class="author">auteur</p>
