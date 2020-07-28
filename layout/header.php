7b86a7e882727cf40db8d0b8867418495332fa4f
Mathieu Bourlard <mathieubourlard01@gmail.com>
2020-07-28 11:39:55 +0200

    avancement 28/07 11:40

--- layout/header.php
+++ layout/header.php
@@ -23,7 +23,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header after connection</title>
 
-    <link rel="stylesheet" href="./src/css/style.css">
+    <link rel="stylesheet" href="../src/css/style.css">
 </head>
 <body>
     <header>
