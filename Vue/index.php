b401997a1305170b2e80f258d145934a04da4ae0
said956 <elmohor.said@gmail.com>
2020-07-29 12:07:11 +0200

    index.php

--- Vue/index.php
+++ Vue/index.php
@@ -1,8 +1,5 @@
 <?php
-include 'config/config.php';
-if(!isset($_SESSION)){
-    session_start();
-}
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -12,27 +9,28 @@ if(!isset($_SESSION)){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 
-    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="../src/css/style.css">
 </head>
 <body>
 <body>
 <?php
 if(isset($_GET['id']) AND $_GET['id'] == $_SESSION['id']){
-    include("layout/header.php");
+    include("../layout/header.php");
 }else{
-    include("layout/header.inc.php");
+    include("../layout/header.inc.php");
 }
 ?>
 <main>
     <div class="feedback">
         <?php date_default_timezone_set('Europe/Paris')?>
         <div class="range">
+
             <?php
             if(isset($_GET['id']) AND $_GET['id'] == $_SESSION['id']){
                 ?>
                 <a href="event.php?id=<?php echo $_SESSION['id']; ?>" class="buttonadd">events</a>
                 <a href="past_event.php?id=<?php echo $_SESSION['id']; ?>" class="buttonadd">past events</a>
-                <a href="create_event.php?id=<?php echo $_SESSION['id']; ?>" class="buttonadd">+ add event</a>
+                <a href="create_event.php?id=<?php echo $_SESSION['id']; ?>" class="buttonadd">+ add even</a>
                 <?php
             }else{
                 ?>
@@ -42,36 +40,14 @@ if(isset($_GET['id']) AND $_GET['id'] == $_SESSION['id']){
             }
             ?>
         </div>
-        <div id="slider">
-            <?php include("slider.php"); ?>
-        </div>
+        <?php include("../Vue/slider.php")?>
         <section class="article">
 
             <i class="fab fa-readme buttonsection"></i>
             <i class="fas fa-pen buttonsection"></i>
             <i class="fas fa-trash buttonsection"></i>
-            <?php
-            $bdd = new PDO('mysql:host=localhost;dbname=jepsen-brite;charset=utf8',
-                                    'root', '');
-            $events = $bdd->query('select id as id, date as date,'
-            . ' titre as titre, auteur as auteur'
-            . ' image as image, description as description, categorie as categorie'
-            . ' from evenement where date > now() limit 10');
-            foreach ($events as $event):
-            echo
-            '<section class="article">
-            <header>
-            <h2>'. $event['titre'] .'</h2>
-            <p>' $event['auteur'] .'</p>
-            <time>'. $event['date'] .'</time>
-            </header>
-            <img  alt="image event" class="imgevent" '. $event['image'] .'> 
-            <p>' . $event['description'] . '</p>
-            <p>' . $event['categorie'] . '</p>
-            </section>
-            <hr />';
-            endforeach;
-            <img src="src/img/téléchargement.jpg" alt="image event" class="imgevent">
+
+            <img src="../src/img/téléchargement.jpg" alt="image event" class="imgevent">
             <h2 class="titre-h2">Bassleader reactivated world <div class="category">concert</div></h2>
             <h5 class="date"><?php echo date('H:i, D d/m/Y')?></h5>
             <p class="description"><strong>description:</strong>
@@ -84,7 +60,7 @@ if(isset($_GET['id']) AND $_GET['id'] == $_SESSION['id']){
             <i class="fas fa-pen buttonsection"></i>
             <i class="fas fa-trash buttonsection"></i>
 
-            <img src="src/img/2016-Daydream-Festival.jpg" alt="image event" class="imgevent">
+            <img src="../src/img/2016-Daydream-Festival.jpg" alt="image event" class="imgevent">
             <h2 class="titre-h2">Daydream festival<div class="category">concert</div></h2>
             <h5 class="date"><?php echo date('H:i, D d/m/Y')?></h5>
             <p class="description"><strong>description:</strong> Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
@@ -96,7 +72,7 @@ if(isset($_GET['id']) AND $_GET['id'] == $_SESSION['id']){
             <i class="fas fa-pen buttonsection"></i>
             <i class="fas fa-trash buttonsection"></i>
 
-            <img src="src/img/dominator.jpg" alt="image event" class="imgevent">
+            <img src="../src/img/dominator.jpg" alt="image event" class="imgevent">
             <h2 class="titre-h2">Dominator 2020<div class="category">concert</div></h2>
             <h5 class="date"><?php echo date('H:i, D d/m/Y')?></h5>
             <p class="description"><strong>description:</strong> Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
@@ -104,7 +80,7 @@ if(isset($_GET['id']) AND $_GET['id'] == $_SESSION['id']){
         </section >
     </div>
 </main>
-<?php include("layout/footer.inc.php");?>
+<?php include("../layout/footer.inc.php");?>
 <script src="https://kit.fontawesome.com/1815b8a69b.js" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ layout/header.php
@@ -0,0 +1,42 @@
+<?php
+if(!isset($_SESSION)){
+    session_start();
+}
+
+include 'config/config.php';
+
+try {
+    $bdd = new PDO($dbdsn, $dbusername, $dbpassword, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+if(isset($_GET['id']) AND $_GET['id'] > 0){
+    $getid = intval($_GET['id']);
+    $requser = $bdd->prepare("SELECT * FROM utilisateur WHERE id = ?");
+    $requser->execute(array($getid));
+    $userinfo = $requser->fetch();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header after connection</title>
+
+    <link rel="stylesheet" href="../src/css/style.css">
+</head>
+<body>
+<header>
+    <div class='avatar'>
+        <a href="./user.php?id=<?php echo $_SESSION['id']; ?>"><img src="./user/avatar/<?php echo $userinfo['avatar']; ?>" alt="avatar" width='45' style="border-radius:22.5px; margin-left: 10px; display: flex; align-self: center; height: auto;"></a>
+    </div>
+    <div class='bouton'>
+        <a href="./index.php?id=<?php echo $_SESSION['id']; ?>" class="logoimg"><img src="./src/img/jepsen_brite.png" alt="logo jepsen-brite"></a>
+        <button class="buttonsearch"><i class="fas fa-search"></i></button>
+        <a href="./logout.php" class='buttonsignup'>Log out</a>
+    </div>
+</header>
+</body>
+</html>
